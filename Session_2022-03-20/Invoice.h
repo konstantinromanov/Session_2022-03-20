868e17a09934f8ea72c69eb7f2116f680da5d9c0
Konstantins Romanovs <konstantin.hvac@gmail.com>
2022-03-20 16:59:52 +0200

    Move Invoice class to the header file

---
+++ Session_2022-03-20/Invoice.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Invoice {
+private:
+	string mTypeNumber;
+	string mDescription;
+	int mQty;
+	int mPrice;
+public:
+	Invoice() {
+
+	}
+
+	Invoice(string typeNumber, string description, int qty) {
+		mTypeNumber = typeNumber;
+		mDescription = description;
+		mQty = qty;
+	};
+
+	Invoice(string typeNumber, string description, int qty, int price) {
+		mTypeNumber = typeNumber;
+		mDescription = description;
+		mQty = qty;
+		mPrice = price;
+	};
+
+	string getTypeNumber() {
+		return mTypeNumber;
+	}
+
+	string getDescription() {
+		return mDescription;
+	}
+
+	int getQty() {
+		return mQty;
+	}
+
+	int getPrice() {
+		return mPrice;
+	}
+
+	void setTypeNumber(string typeNumber) {
+		mTypeNumber = typeNumber;
+	}
+
+	void setDescription(string description) {
+		mDescription = description;
+	}
+
+	void setQty(int qty) {
+		mQty = qty;
+	}
+
+	void setPrice(int price) {
+		mPrice = price;
+	}
+};
--- Session_2022-03-20/Session_2022-03-20.cpp
+++ Session_2022-03-20/Session_2022-03-20.cpp
@@ -3,67 +3,11 @@
 
 #include <iostream>
 #include <vector>
+#include "Invoice.h"
 using namespace std;
 
 template<typename T>
 void checkPalindrome(vector<T> col);
-
-class Invoice {
-private:
-	string mTypeNumber;
-	string mDescription;
-	int mQty;
-	int mPrice;
-public:
-	Invoice() {
-
-	}
-
-	Invoice(string typeNumber, string description, int qty) {
-		mTypeNumber = typeNumber;
-		mDescription = description;
-		mQty = qty;
-	};
-
-	Invoice(string typeNumber, string description, int qty, int price) {
-		mTypeNumber = typeNumber;
-		mDescription = description;
-		mQty = qty;
-		mPrice = price;
-	};
-
-	string getTypeNumber() {
-		return mTypeNumber;
-	}
-
-	string getDescription() {
-		return mDescription;
-	}
-
-	int getQty() {
-		return mQty;
-	}
-
-	int getPrice() {
-		return mPrice;
-	}
-
-	void setTypeNumber(string typeNumber) {
-		mTypeNumber = typeNumber;
-	}
-
-	void setDescription(string description) {
-		mDescription = description;
-	}
-
-	void setQty(int qty) {
-		mQty = qty;
-	}
-
-	void setPrice(int price) {
-		mPrice = price;
-	}
-};
 
 
 int main()
